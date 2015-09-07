3a3f513ee85e6239c5fc1a37597e508b84927ed4
Daniel Phillips <daniel.phillips@thetrainline.com>
2015-09-07 21:07:16 +0100

    Add framework target, remove static library

---
+++ MBProgressHUD/MBProgressHUD.h
@@ -0,0 +1,19 @@
+//
+//  MBProgressHUD.h
+//  MBProgressHUD
+//
+//  Created by Daniel on 07/09/2015.
+//  Copyright © 2015 Matej Bukovinski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MBProgressHUD.
+FOUNDATION_EXPORT double MBProgressHUDVersionNumber;
+
+//! Project version string for MBProgressHUD.
+FOUNDATION_EXPORT const unsigned char MBProgressHUDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MBProgressHUD/PublicHeader.h>
+
+
